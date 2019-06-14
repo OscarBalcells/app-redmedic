03bb9a479bf6a192a386703864eabe6b7fbcea40
OscarBalcells <balcells.oscar@gmail.com>
2019-06-14 20:21:20 +0200

    He cambiado la estructura de carpetas

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import '../css/App.css';
+import Lateral from "./Lateral.js";
+
+const appStyle = {
+	color: "blue",
+	borderTop: "1px solid red",
+}
 
 class App extends React.Component {
   render() {
-		console.log("Patatas");
 		return(
-			<h1>Esta es mi app</h1>
+			<div style={appStyle}>
+				<h1>Esta es mi app</h1>
+				<Lateral />
+			</div>
 		);
 	}
 }
